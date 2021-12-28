da52ad6a4441e54bbce9bf96fde918d6595fcc6d
kumar-devesh <kumar_d@cs.iitr.ac.in>
2021-12-28 12:48:57 +0530

    fixed bug in decryption module

--- decrypt.py
+++ decrypt.py
@@ -2,23 +2,60 @@ import encrypt as encrypt
 from encrypt import random_key
 import numpy as np
 import utils
-from ecc import key_length, public_key
-
+import ecc
+from ecc import G, key_length, public_key
+from ecc import secret_key as private_key
+from ecc import bitcoin_curve as BC
 
 def group_pixels(channel, key_length):
   members = key_length//8 - 1
+  group_length = 0
   b = []
   grp = ""
+  found = []
   for i in range(len(channel)):
+    group_length+=1
+    found.append(channel[i])
     grp+=utils.get_bin(channel[i], 8)
-    if i%(members+1)==0 and i!=0:
-      b.append(int(grp, 2))
-      grp = ""
-
-  b.append(int(grp.zfill(key_length-8), 2))
-  # print(b)
-
+    if group_length==32:
+        b.append(int(grp, 2))
+        found = []
+        grp = ""
+        group_length = 0
   return b
+
+def make_points_and_decrypt(grouped_pixels, public_key):
+  keyG = random_key*G
+  keyPv = private_key*keyG
+  PC = []
+  for i in range(0, len(grouped_pixels), 2):
+    x = grouped_pixels[i]
+    y = BC.p - grouped_pixels[i+1]
+    point = ecc.Point(ecc.bitcoin_curve, x, y)
+    point = point + keyPv
+    point.y = BC.p - point.y
+    PC.append(point)
+
+  return PC
+
+def get_channel_for_cipher_image(PC, width):
+  pixel_values = []
+  for point in PC:
+    x, y = point.x, point.y
+    bins = utils.get_bin(x, 256)
+    nums = [int(bins[i:i+8], 2) for i in range(0, len(bins), 8)]
+    pixel_values.append(nums)
+
+    bins = utils.get_bin(y, 256)
+    nums = [int(bins[i:i+8], 2) for i in range(0, len(bins), 8)]
+    pixel_values.append(nums)
+
+  for i in pixel_values:
+    while (len(i)!=32):
+      i.append(0)
+
+  pixel_values = np.asarray(pixel_values).flatten().reshape(width, -1)
+  return(pixel_values)
 
 
 def decrypt_image(filepath):
@@ -26,20 +63,10 @@ def decrypt_image(filepath):
   pixels_list = encrypt.extract_channels(image, channels)
 
   original_channels = []
-
   for channel_pixel in pixels_list:
     grouped_pixels = group_pixels(channel_pixel, key_length)
-    # secret_points = make_points_and_encrypt(grouped_pixels, public_key)
-    # sec_channel = get_channel_for_cipher_image(secret_points, width)
-    # secret_channels.append(sec_channel)
-
+    secret_points = make_points_and_decrypt(grouped_pixels, public_key)
+    sec_channel = get_channel_for_cipher_image(secret_points, width)
+    original_channels.append(sec_channel)
+    
   return np.dstack((tuple(original_channels)))
-
-
-decrypted_image = decrypt_image('enc.png')
-print(decrypted_image)
-
-# from matplotlib import pyplot as plt
-# # plt.imshow(encrypted_image, interpolation='nearest')
-# # plt.show()
-# plt.savefig('enc.png')
--- encrypt.py
+++ encrypt.py
@@ -36,8 +36,6 @@ def group_pixels(channel, key_length):
     grp+=get_bin(channel[i], 8)
     if group_length==32:
       b.append(grp)
-      if (i==31):
-        print(found, b)
       grp = ""
       group_length = 0
 
@@ -56,13 +54,7 @@ def make_points_and_encrypt(grouped_pixels, public_key):
   PC = []
   for i in range(0, len(grouped_pixels), 2):
     point = ECC.Point(ECC.bitcoin_curve, int(grouped_pixels[i], 2), int(grouped_pixels[i+1], 2))
-    if (i<3):
-      print(point)
-      # print("point IS on the curve: ", (point.y**2 - point.x**3 - 7) % bitcoin_curve.p == 0)
     point = point + keyPb
-    if (i<3):
-      # print("point IS on the curve: ", (point.y**2 - point.x**3 - 7) % bitcoin_curve.p == 0)
-      print(point)
     PC.append(point)
 
   return PC
@@ -77,53 +69,30 @@ def get_channel_for_cipher_image(PC, width):
     nums = [int(bins[i:i+8], 2) for i in range(0, len(bins), 8)]
     pixel_values.append(nums)
     if (ok):
-      print('Final nums and bins')
-      print(nums, bins)
       ok = False
-    # print(nums, bins)
-    # print(len(nums), len(bins))
+
     bins = get_bin(y, 256)
     nums = [int(bins[i:i+8], 2) for i in range(0, len(bins), 8)]
     pixel_values.append(nums)
-    # print(nums, bins)
-    # print(len(nums), len(bins))
-    # print(pixel_values)
+
 
   for i in pixel_values:
     while (len(i)!=32):
       i.append(0)
 
-  # print(len(pixel_values))
   pixel_values = np.asarray(pixel_values).flatten().reshape(width, -1)
-  # print(pixel_values.shape)
-  # print(pixel_values)
-
-  return(pixel_values)
+  return pixel_values
 
 
 def encrypt_image(filepath):
   image, channels, length, width = load_image(filepath)
   pixels_list = extract_channels(image, channels)
-  print(pixels_list[0][:32])
   secret_channels = []
 
   for channel_pixel in pixels_list:
-    # print(len(channel_pixel))
     grouped_pixels = group_pixels(channel_pixel, key_length)
-    # print(len(grouped_pixels))
     secret_points = make_points_and_encrypt(grouped_pixels, public_key)
-    # print(secret_points)
     sec_channel = get_channel_for_cipher_image(secret_points, width)
     secret_channels.append(sec_channel)
 
   return np.dstack((tuple(secret_channels)))
-
-encrypted_image = encrypt_image('lena.png')
-# print(encrypted_image)
-image, channels, length, width = load_image('lena.png')
-# print(*extract_channels(image, channels)[0][:320], sep=", ")
-
-from PIL import Image
-im = Image.fromarray((encrypted_image).astype(np.uint8))
-im.save('enc.png')
-
